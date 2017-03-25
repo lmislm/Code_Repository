5fb7cd13c55bb27610ac01241b8b67c94559379c
lmislm <lmjustlm@163.com>
2017-03-25 21:22:10 +0800

    add call方法
    加call方法，对比apply和 call用法

--- js/maxNumber_arguments.js
+++ js/maxNumber_arguments.js
@@ -5,4 +5,7 @@
 function maxNumber() {
     return Math.max.apply(null,arguments)
 }
+// function maxNumber(num1,num2) {
+//     return Math.max.call(null,num1,num2);
+// }
 console.log(maxNumber(4,3));
