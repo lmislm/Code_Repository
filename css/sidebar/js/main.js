a9e28101ee282ccf4838c1d5df85cc57fcf21a5f
lmislm <lmjustlm@163.com>
2017-06-10 17:47:59 +0800

    封装成JQuery插件

--- css/sidebar/js/main.js
+++ css/sidebar/js/main.js
@@ -7,22 +7,38 @@ requirejs.config({
         jquery: '../node_modules/.3.2.1@jquery/dist/jquery'
     }
 });
-requirejs(['jquery','scrollto'], function ($,scrollto) {
-    //使用scrollto函数，模块化一下
-    var scroll = new scrollto.ScrollTo({
-        //即scrollto函数中return的ScrollTo
-        dest: 500,
-        speed: 2000
+// requirejs(['jquery','scrollto'], function ($,scrollto) {
+requirejs(['jquery','backtop'], function ($,backtop) {
+
+    // new backtop.BackTop($('#backTop'), {
+    //     mode: 'move', //move  ||  quickBack
+    //     pos: 100,
+    //     speed: 2000
+    // });
+
+    //注册成插件
+    $('#backTop').backtop({
+        mode: 'move'
     });
 
-    $('body').css('background-color', 'red');
-    //unndefined the animate,
-    //$.proxy，第二个参数即this指向实例对象
-    $('#backTop').on('click', $.proxy(scroll.move,scroll)); //替换quickBack即可
+
+
+
+    //使用scrollto函数，模块化一下
+    // var scroll = new scrollto.ScrollTo({
+    //     //即scrollto函数中return的ScrollTo
+    //     dest: 0,
+    //     speed: 2000
+    // });
+    //
+    // $('body').css('background-color', 'red');
+    // //unndefined the animate,
+    // //$.proxy，第二个参数即this指向实例对象
+    // $('#backTop').on('click', $.proxy(scroll.move,scroll)); //替换quickBack即可
     // $(window).on('scroll',function () {
     //     checkPosition($(window).height()); //匿名函数调用,判断可视高度
     // });//检查位置
-    //solve bug,加载的时候就执行函数。进行判断
+    // //solve bug,加载的时候就执行函数。进行判断
     // checkPosition($(window).height()); //匿名函数调用,判断可视高度
     //
     // function move() {
@@ -34,7 +50,7 @@ requirejs(['jquery','scrollto'], function ($,scrollto) {
     // function quickBack() {
     //     $('html, body').scrollTop(0);
     // }
-    //
+
     // function checkPosition(pos) {
     //     //每次滚动条滚动的时候执行，bug，刷新时重新显示返回顶部
     //     if ($(window).scrollTop() > pos) {
--- css/sidebar/js/scrollto.js
+++ css/sidebar/js/scrollto.js
@@ -4,21 +4,31 @@
 define(['jquery'],function ($) {
     function ScrollTo(opts) {
         //默认参数
-        this.opts = $.extend({},ScrollTo.DEFAUKTS, opts);// 覆盖新的对象
+        this.opts = $.extend({},ScrollTo.DEFAULTS, opts);// 覆盖新的对象
         this.$el = $('html, body');
     }
     ScrollTo.prototype.move = function () {
-        var opts = this.opts;
-        this.$el.animate({ //this指向问题  ,animate的性能问题
-            scrollTop: this.opts.dest
-        },opts.speed);
+        var opts = this.opts,
+            dest = opts.dest;
+
+        if($(window).scrollTop() !=  dest) {
+            if(!this.$el.is(':animated')) {
+                this.$el.animate({ //this指向问题  ,animate的性能问题
+                    scrollTop: dest
+                }, opts.speed);
+            }
+        }
     }
     ScrollTo.prototype.quickBack = function () {
-        this.$el.scrollTop(this.opts.dest);
+        var dest = this.opts.dest;
+
+        if($(window).scrollTop() != dest) {
+            this.$el.scrollTop(dest);
+        }
     }
-    ScrollTo.DEFAUKTS = {
+    ScrollTo.DEFAULTS = {
         dest: 0, //destination,
-        speed: 80
+        speed: 800
     };
 
     return {
