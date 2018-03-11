5270cbdb28a96588a09d85a211e941a497cc08dc
lmislm <wszdlm@gmail.com>
2018-03-11 22:41:23 +0800

    es6 modern

---
+++ js/js_note/app.js
@@ -0,0 +1,53 @@
+// ------- constructor ---------
+// const person = {
+//     name: '张三',
+// };
+
+// const newPerson = {
+//     ...person,
+//     age: 28
+// };
+// console.log(newPerson);
+
+// ------- rest ------
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// } 
+
+// console.log(filter(1,2,3));
+
+// ------Destructuring ---------
+
+// [a, b] = ['张三', '李四']
+// console.log(a)
+// console.log(b)   
+// const numbers = [1,2,3];
+// [num1, , num3] = numbers;
+// console.log(num1, num3);
+
+
+
+// Reference and Primitive Types Refresher
+
+// const person = {
+//     name: '张三'
+// };
+// // const secondPerson = person;
+// person.name = '李四';
+// const secondPerson = {
+//     ...person
+// };
+// person.name = '李四';
+// console.log(secondPerson)
+
+// --------- Array function -------
+// const numbers = [1, 2, 3];
+
+// const doubleNumArray = numbers.map((num) => {
+//     return num * 2;
+// });
+
+// console.log(numbers);
+// console.log(doubleNumArray);
+
+
